5f363e909d8693d228db80aacf099ad5f4773372
VitorEduardo7 <161628040+VitorEduardo7@users.noreply.github.com>
2025-03-13 14:44:42 -0300

    Aula04

---
+++ aula 04/exercicio1.php
@@ -0,0 +1,18 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>exercicio 1</title>
+</head>
+<body>
+    <h1>NOTAS ALUNOS>
+    <form action="exericio1.php" method="get" >
+     <p>Digite seu nome: <input type="text" required></p>
+     <p>Digite sua primeira nota: <input type="number" name="Nota1" id=""></p>
+     <p>Digite sua segunda nota: <input type="number" name="Nota2" id="">/p>
+     <p>Digite sua terceira nota: <input type="number" name="Nota3" id=""></p>
+    </form>
+
+</body>
+</html>
